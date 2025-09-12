8527dd5a0b50681e314ee2f00496f6b03689ceb3
Radhikavennapusala <radhikavennapusala@gmail.com>
2025-09-12 20:17:23 +0530

    Delete 2.digit or number.py

--- 2.digit or number.py
+++
@@ -1,9 +0,0 @@
-num=int(input("enter the integer"))
-#if_else
-if(num>=-9 and num<=9):
-   print("digit")
-else:
-     print("number")
-#ternary operator
-res="digit" if(num>=-9 and num<=9) else "number"
-print(res)
