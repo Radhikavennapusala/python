9a349942e494564c32d86e67b7f3a029feca3524
Radhikavennapusala <radhikavennapusala@gmail.com>
2025-09-15 09:49:12 +0530

    Add files via upload

---
+++ Day1/14.even or odd num.py
@@ -0,0 +1,6 @@
+num=int(input("enter the integer"))
+#if_else
+if(num%2==0):
+    print(num,"is the even number")
+else:
+    print(num,"is an odd number")
---
+++ Day1/16.fizzbuzz.py
@@ -0,0 +1,7 @@
+n=int(input("enter the integer value"))
+if(n%3==0 and n%5==0):
+    print("fizzbuzz")
+elif(n%3==0):
+    print("fizz")
+elif(n%5==0):
+    print("buzz")
